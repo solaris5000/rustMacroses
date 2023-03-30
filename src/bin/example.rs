0613e8e8c7e3180fe078c8b21d15bb6ba0544158
solaris5000 <solaris5000tv@gmail.com>
2023-03-30 12:22:00 +0600

    a

--- src/bin/example.rs
+++ src/bin/example.rs
@@ -1,12 +1,23 @@
-use proc_macro_example::Count;
+use proc_macro_example::Counte;
+use proc_macro_example::Identy;
+use proc_macro_example::HelloMacro;
 
-#[derive(Count)]
+//#[derive(Counte, Identy)]
 enum Example {
     Variant1,
     Variant2,
     Variant3,
 }
 
+#[derive(HelloMacro)]
+struct Pancakes;
+
+
+
 fn main() {
-    println!("Our enum has {} variants", Example::count());
+    let a = Example::Variant1;
+    Pancakes::;
+   // println!("Our enum has {} variants", Example::count());
+
+   // println!("Out enum a has id {:?}", Example::identy());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,68 @@ use quote::quote;
 /// Example of user-defined [derive mode macro][1]
 ///
 /// [1]: https://doc.rust-lang.org/reference/procedural-macros.html#derive-mode-macros
-#[proc_macro_derive(Count)]
+#[proc_macro_derive(Counte)]
 pub fn count(input: TokenStream) -> TokenStream {
     let macro_input = parse_macro_input!(input as DeriveInput);
 
-    // Your code here
+     let res = &macro_input.ident;
+     let data = &macro_input.data;
+
+    let count = match  *data {
+
+        Data::Enum(ref dat) => dat.variants.len(),
+        Data::Struct(_) | Data::Union(_) => unimplemented!(),
+    };
 
     quote!(
-        // Your code here
+        impl #res {
+            fn count() -> usize {
+                3 as usize
+            }
+        }
     ).into()
+}
+
+#[proc_macro_derive(Identy)]
+pub fn identy(input: TokenStream) -> TokenStream {
+    let macro_input = parse_macro_input!(input as DeriveInput);
+
+    let var = &macro_input.ident;
+    let raw_data = &macro_input.data;
+
+    let id = match *raw_data {
+        Data::Enum(ref data) => { data.enum_token },
+        Data::Struct(_) | Data::Union(_) => unimplemented!(),
+    };
+
+    quote!(
+        impl #var {
+            fn identy() -> usize {
+                #id
+            }
+        }
+    ).into()
+}
+
+
+#[proc_macro_derive(HelloMacro)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate
+    let ast = syn::parse(input).unwrap();
+
+    // Build the trait implementation
+    impl_hello_macro(&ast)
+}
+
+fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
 }
